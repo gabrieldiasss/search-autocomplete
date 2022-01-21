247c74aa45e7063e850768bda03e1391b1fe6041
Gabriel  Dias <gabrieldiascorrea7@gmail.com>
2022-01-21 16:52:42 -0300

    first commit

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Components() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Components;
