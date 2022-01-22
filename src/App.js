c6ca55d1e6576fb4aefcd09dd9aeb266127be9db
Gabriel  Dias <gabrieldiascorrea7@gmail.com>
2022-01-22 04:50:18 -0300

    project finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from './Components/SearchBar.js'
+import Users from './db.json'
+
+import './App.css'
 
 function App() {
-	
+
 	return (
-		
+		<div>
+			<SearchBar data={Users} />
+		</div>
   	);
 }
 
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,14 +1,72 @@
-import React from 'react';
+import './SearchBar.css'
 
-// import { Container } from './styles';
+import { GoSearch } from 'react-icons/go'
+import { AiOutlineClose } from 'react-icons/ai'
 
-function Components() {
+import { IconContext } from 'react-icons'
+import { useEffect, useState } from 'react'
+
+function SearchBar({ data }) {
+
+    const [inputSearch, setInputSearch] = useState("")
+    const [filterSearch, setFilterSearch] = useState([])
+
+    const handleFilter = (event) => {
+        setInputSearch(event.target.value)
+
+        const newFilter = data.filter(value => {
+            return value.title.toLowerCase().includes(inputSearch.toLowerCase())
+        })
+
+        setFilterSearch(newFilter)
+
+    }
+
+    useEffect(() => {
+
+        if (inputSearch === "") {
+            setFilterSearch([])
+        }
+
+    }, [inputSearch])
+
+    function handleClickAutoComplete(value) {
+        setInputSearch(value.title)
+        setFilterSearch([])
+    }
+
+    function clearText() {
+        setInputSearch("")
+        setFilterSearch([])
+    }
 
     return (
-        <div>
-            
+        <div className='search' >
+
+            <div className='searchInputs' >
+                <IconContext.Provider value={{ color: "#B8B8B8", size: "30px"}}>
+                    <GoSearch />
+
+                    <input type="text" placeholder='Pesquisar...' value={inputSearch} onChange={handleFilter} />
+
+                    {inputSearch !== "" ? <AiOutlineClose onClick={clearText} /> : ""}
+                </IconContext.Provider>
+            </div>
+
+            {filterSearch.length !== 0 &&
+                <div className='dataResult'>
+                    {filterSearch.slice(0, 15).map(value => (
+                        <div key={value.id} className='dataItem' onClick={() => handleClickAutoComplete(value)}>
+                            <IconContext.Provider value={{ color: "#B8B8B8", size: "22px" }}>
+                                <GoSearch />
+                            </IconContext.Provider>
+                            <p>{value.title}</p>
+                        </div>
+                    ))}
+                </div>
+            }
         </div>
     )
 }
 
-export default Components;
+export default SearchBar
